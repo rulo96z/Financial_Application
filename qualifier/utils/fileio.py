920a1696225fa69b19539664660ac0c84404491c
Raul Nogales <rulo.nogales@nogalesinvestments.com>
2021-10-03 06:36:55 -0700

    update fileio.py with save_csv function

--- qualifier/utils/fileio.py
+++ qualifier/utils/fileio.py
@@ -6,7 +6,7 @@ This contains a helper function for loading and saving CSV files.
 """
 import csv
 
-
+# Defenetion to load csv_file:
 def load_csv(csvpath):
     """Reads the CSV file from path provided.
 
@@ -28,4 +28,19 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+# Defenition to save csv_file:
+def save_csv(csvpath, data, header=None):
+    """Saves the CSV file from path provided.
+    Args:
+        csvpath (Path): The CSV file path.
+        data (list of lists): A list of the rows of data for the CSV file.
+        header (list): An optional header for the CSV.
+    """   
+    with open(csvpath, "w", newline="") as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',')
+        if header:
+            csvwriter.writerow(header)
+        csvwriter.writerows(data)
+
 
